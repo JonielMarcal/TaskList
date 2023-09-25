b9fe22c7d4d20ebdd91d0a10ddf1545fdf6b5d42
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2023-09-25 20:57:07 -0300

    Adicionando funções no script.js

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const input = document.querySelector(".textInput");
 const button = document.querySelector(".btnInclud");
+const ULs = document.querySelector(".listaCompleta");
 
 let tarefas = [];
 
 const addTarefa = () => {
-  tarefas.push(input.value);
+  tarefas.push({
+    tarefa: input.value,
+    concluida: false,
+  });
   input.value = "";
+  mostrarTarefa();
+};
+
+const mostrarTarefa = () => {
+  novaLista = "";
+
+  tarefas.map((item, index) => {
+    novaLista =
+      novaLista +
+      `
+        <li class="lista">
+            <button onclick="concluirTarefa(${index})">Concluir</button>
+            <p>${item}</p>
+            <button onclick="excluirTarefa(${index})">Excluir</button>
+        </li>
+        `;
+  });
+  ULs.innerHTML = novaLista;
+};
+
+const excluirTarefa = (index) => {
+  tarefas.splice(index, 1);
+  mostrarTarefa();
+};
+
+const concluirTarefa = (index) => {
+  console.log("Posicao", index);
 };

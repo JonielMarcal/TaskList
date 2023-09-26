d0b34396ae1a37d57a784219a3886600ed86d04b
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2023-09-26 11:10:50 -0300

    add metodo trim() para add os espaços em branco no input, evitando add tarefas em branco.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const ULs = document.querySelector(".listaCompleta");
 let tarefas = [];
 
 const addTarefa = () => {
-  if (input.value === " ") {
+  if (input.value.trim() === "") {
     alert("Favor, informe uma tarefa!");
   } else {
     tarefas.push({
@@ -26,13 +26,14 @@ const mostrarTarefa = () => {
       `
         <li class="lista ${item.concluida ? "none" : "lista"}">
             <button class="buttonConcluir" onclick="concluirTarefa(${index})">Concluir</button>
-            <p>${item.nome}</p>
+            <span>${item.nome}</span>
             <button class="buttonExcluir" onclick="excluirTarefa(${index})">Excluir</button>
         </li>
         
         `;
   });
   ULs.innerHTML = novaLista;
+  localStorage.setItem("lista", JSON.stringify(tarefas));
 };
 
 const excluirTarefa = (index) => {
@@ -44,3 +45,10 @@ const concluirTarefa = (index) => {
   tarefas[index].concluida = !tarefas[index].concluida;
   mostrarTarefa();
 };
+
+const salvarLocalStorafe = () => {
+  const tarefasLocalStorage = localStorage.getItem("lista");
+  tarefas = JSON.parse(tarefasLocalStorage);
+  mostrarTarefa();
+};
+salvarLocalStorafe();
